781e677042849858f9310c452ced505d34801635
Marcel Veldhuizen <marcel@subbot.net>
2016-07-31 14:46:47 +0200

    Switched to using binary sensor reporting for now.
    So far I'm not having much luck with the Notification command class which Strips uses by default.
    Added some crappy images as well. Still waiting for a reply from Sensative whether I can include their logos etc.

--- drivers/1101011/driver.js
+++ drivers/1101011/driver.js
@@ -1,31 +1,62 @@
 "use strict";
 
-const path			= require('path');
-const ZwaveDriver	= require('homey-zwavedriver');
+const path          = require('path');
+const ZwaveDriver   = require('homey-zwavedriver');
 
 // http://www.pepper1.net/zwavedb/device/831
 
 module.exports = new ZwaveDriver( path.basename(__dirname), {
-	capabilities: {
-		'alarm_contact': {
-            // TODO: COMMAND_CLASS_ALARM doesn't seem to be recognized
-			'command_class'				: 'COMMAND_CLASS_ALARM',
-			'command_get'				: 'ALARM_GET',
-			'command_report'			: 'ALARM_REPORT',
-			'command_report_parser'		: function( report ){
-                console.log(JSON.stringify(report));
-				return report['Sensor Value'] === 'detected an event';
-			}
-		},
-		'measure_battery': {
-			'command_class'				: 'COMMAND_CLASS_BATTERY',
-			'command_get'				: 'BATTERY_GET',
-			'command_report'			: 'BATTERY_REPORT',
-			'command_report_parser'		: function( report ) {
-                console.log(JSON.stringify(report));
-				if( report['Battery Level'] === "battery low warning" ) return 1;
-				return report['Battery Level (Raw)'][0];
-			}
-		}
-	}
+    debug: true,
+    capabilities: {
+        'alarm_contact': {
+            'command_class'             : 'COMMAND_CLASS_SENSOR_BINARY',
+            'command_get'               : 'SENSOR_BINARY_GET',
+            'command_report'            : 'SENSOR_BINARY_REPORT',
+            'command_report_parser'     : function( report ){
+                //console.log(JSON.stringify(report));
+                return report['Sensor Value (Raw)'][0] > 0;
+            }
+        },        
+        // 'alarm_contact': {
+        //     'command_class'             : 'COMMAND_CLASS_NOTIFICATION',
+        //     'command_get'               : 'NOTIFICATION_GET',
+        //  'command_get_parser'        : function() {
+        //      return {
+        //          'V1 Alarm Type': 6,
+        //          'Notification Type': 'Home Security',
+        //          'Event': 6
+        //      }
+        //  },            
+        //     'command_report'            : 'NOTIFICATION_REPORT',
+        //     'command_report_parser'     : function(report) {
+        //         console.log(JSON.stringify(report));
+        //      return report['Alarm Level'] > 0;     
+        //     }
+        // },
+        'measure_battery': {
+            'command_class'             : 'COMMAND_CLASS_BATTERY',
+            'command_get'               : 'BATTERY_GET',
+            'command_report'            : 'BATTERY_REPORT',
+            'command_report_parser'     : function(report) {
+                if( report['Battery Level'] === "battery low warning" ) return 1;
+                return report['Battery Level (Raw)'][0];
+            }
+        }
+    },
+    settings: {
+        "report_type": {
+            "index": 1,
+            "size": 1,
+            "parser": function(input) {
+                return new Buffer([ parseInt(input) ]);
+            }
+        },        
+        "led_indication": {
+            "index": 2,
+            "size": 1,
+            "parser": function(input) {
+                return new Buffer([input ? 1 : 0 ]);
+            }
+        }
+    } 
 })
